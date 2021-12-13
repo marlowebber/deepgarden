ed3c076b6693b164f845bf87a0f0ad089720121f
marlowebber <marlo.m.webber@gmail.com>
2021-12-13 22:33:50 +1100

    initial work to create a cellular automata in the deepsea engine

---
+++ CMakeFiles/deepgarden.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deepgarden.dir/src/main.cpp.o"
+  "deepgarden"
+  "deepgarden.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/deepgarden.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
